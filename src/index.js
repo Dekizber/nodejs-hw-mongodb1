4e6705181f04bb0f37025d9bbc8c3b71bfd8e282
dekizber <dekizber@gmail.com>
2024-10-08 14:21:55 +0300

    added json db

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import express from 'express';
-// import mongoose from 'mongoose';
+
+import movies from './db/movies.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
+app.get('/movies', (req, res) => {
+    res.json(movies);
 });
 
-app.get('/contacts', (req, res) => {
-    console.log(req.method);
-    console.log(req.url);
-    res.send('<h1>Contacts page</h1>');
-})
+
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello World!</h1>');
+// });
+
+// app.get('/contacts', (req, res) => {
+//     console.log(req.method);
+//     console.log(req.url);
+//     res.send('<h1>Contacts page</h1>');
+// });
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
-
