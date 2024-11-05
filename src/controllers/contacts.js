af1684417d280a4a42829f970f3328dc19851682
dekizber <dekizber@gmail.com>
2024-11-05 15:03:53 +0200

    HW 6 working

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -2,10 +2,14 @@ import createHttpError from 'http-errors';
 import * as path from 'node:path';
 
 import * as contactServices from '../services/contacts.js';
-import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
 
 // import { parseSortParams } from '../utils/parseSortParams.js';
 // import { sortByListContact } from '../db/models/Contact.js';
+import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { env } from '../utils/env.js';
+
+const enable_cloudinary = env('ENABLE_CLOUDINARY') === 'true';
 
 export const getContacts = async (req, res) => {
     const { page, perPage, sortBy, sortOrder } = req.query;
@@ -27,6 +31,7 @@ export const getContactById = async (req, res, next) => {
     const { _id: userId } = req.user;
     const data = await contactServices.getContact({ _id: contactId, userId });
 
+
     if (!data) {
         throw createHttpError(404, `Contact not found`);
     }
@@ -42,9 +47,16 @@ export const addContact = async (req, res) => {
     const { _id: userId } = req.user;
     let photo;
     if (req.file) {
-        await saveFileToUploadDir(req.file, 'photos');
-        photo = path.join('photos', req.file.filename);
+        if (enable_cloudinary) {
+            photo = await saveFileToCloudinary(req.file);
+        } else {
+            await saveFileToUploadDir(req.file, 'photos');
+            photo = path.join('photos', req.file.filename);
+        }
+
     }
+
+
 
     const data = await contactServices.addContact({ ...req.body, photo, userId });
 
@@ -72,7 +84,17 @@ export const addContact = async (req, res) => {
 export const patchContact = async (req, res) => {
     const { contactId } = req.params;
     const { _id: userId } = req.user;
-    const result = await contactServices.updateContact({ _id: contactId, userId }, req.body);
+    let photo;
+
+    if (req.file) {
+        if (enable_cloudinary) {
+            photo = await saveFileToCloudinary(req.file);
+        } else {
+            await saveFileToUploadDir(req.file, 'photos');
+            photo = path.join('photos', req.file.filename);
+        }
+    }
+    const result = await contactServices.updateContact({ _id: contactId, userId }, { ...req.body, photo });
 
     if (!result) {
         throw createHttpError(404, `Contact not found`);
